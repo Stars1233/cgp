faa8cbda42d012715fc28f6836a87fa3bd98e559
Soares Chen <soares.chen@maybevoid.com>
2026-02-18 11:37:01 +0100

    Tests and Improvements for `#[cgp_fn]` (#198)
    * Apply abstract type alias to other trait bounds
    
    * Allow only one mutable implicit argument
    
    * Disallow mutable implicit variable
    
    * Add multi-args test
    
    * Explicitly annotate implicit variable types

--- crates/cgp-macro-lib/src/cgp_fn/parse_implicits.rs
+++ crates/cgp-macro-lib/src/cgp_fn/parse_implicits.rs
@@ -2,7 +2,8 @@ use std::mem;
 
 use syn::punctuated::Punctuated;
 use syn::token::Comma;
-use syn::{Attribute, FnArg, Meta, Pat, PatType, Receiver};
+use syn::visit::{self, Visit};
+use syn::{Attribute, FnArg, Meta, Pat, PatIdent, PatType, Receiver};
 
 use crate::cgp_fn::ImplicitArgField;
 use crate::derive_getter::parse_field_type;
@@ -11,38 +12,23 @@ pub fn extract_implicits_args(
     receiver: &Receiver,
     args: &mut Punctuated<FnArg, Comma>,
 ) -> syn::Result<Vec<ImplicitArgField>> {
+    let implicit_fn_args = extract_implicit_args(args);
+
+    if receiver.mutability.is_some() && implicit_fn_args.len() > 1 {
+        return Err(syn::Error::new_spanned(
+            &args,
+            "Only one mutable implicit argument is allowed when self is mutable",
+        ));
+    }
+
     let mut implicit_args = Vec::new();
 
-    let process_args = mem::take(args);
-
-    for mut arg in process_args.into_iter() {
-        if let Some(implicit_arg) = try_parse_implicit_arg(receiver, &mut arg)? {
-            implicit_args.push(implicit_arg);
-        } else {
-            args.push(arg);
-        }
+    for arg in implicit_fn_args {
+        let spec = parse_implicit_arg(receiver, &arg)?;
+        implicit_args.push(spec);
     }
 
     Ok(implicit_args)
-}
-
-pub fn try_parse_implicit_arg(
-    receiver: &Receiver,
-    arg: &mut FnArg,
-) -> syn::Result<Option<ImplicitArgField>> {
-    if let FnArg::Typed(arg) = arg {
-        let attrs = mem::take(&mut arg.attrs);
-        for attr in attrs {
-            if is_implicit_attr(&attr) {
-                let spec = parse_implicit_arg(receiver, arg)?;
-                return Ok(Some(spec));
-            } else {
-                arg.attrs.push(attr);
-            }
-        }
-    }
-
-    Ok(None)
 }
 
 pub fn parse_implicit_arg(receiver: &Receiver, arg: &PatType) -> syn::Result<ImplicitArgField> {
@@ -50,24 +36,89 @@ pub fn parse_implicit_arg(receiver: &Receiver, arg: &PatType) -> syn::Result<Imp
         return Err(syn::Error::new_spanned(&arg.pat, "Expected an identifier"));
     };
 
-    let arg_type = arg.ty.as_ref();
+    if has_mut_pattern(&arg.pat) {
+        return Err(syn::Error::new_spanned(
+            &arg.pat,
+            "Mutable variables are not allowed in implicit arguments. (Explicitly clone a `&` reference if you want a mutable local copy of the value)",
+        ));
+    }
+
+    let arg_type = arg.ty.as_ref().clone();
+
     let field_mut = receiver.mutability;
 
-    let (field_type, field_mode) = parse_field_type(arg_type, &field_mut)?;
+    let (field_type, field_mode) = parse_field_type(&arg_type, &field_mut)?;
 
     let spec = ImplicitArgField {
         field_name: pat_ident.ident.clone(),
         field_type,
         field_mut,
         field_mode,
+        arg_type,
     };
 
     Ok(spec)
+}
+
+pub fn extract_implicit_args(args: &mut Punctuated<FnArg, Comma>) -> Vec<PatType> {
+    let mut implicit_args = Vec::new();
+
+    let process_args = mem::take(args);
+
+    for arg in process_args.into_iter() {
+        if let FnArg::Typed(mut arg) = arg {
+            if is_implicit_arg(&mut arg) {
+                implicit_args.push(arg);
+            } else {
+                args.push(FnArg::Typed(arg));
+            }
+        } else {
+            args.push(arg);
+        }
+    }
+
+    implicit_args
+}
+
+pub fn is_implicit_arg(arg: &mut PatType) -> bool {
+    let mut res = false;
+
+    let attrs = mem::take(&mut arg.attrs);
+
+    for attr in attrs {
+        if is_implicit_attr(&attr) {
+            res = true;
+        } else {
+            arg.attrs.push(attr);
+        }
+    }
+
+    res
 }
 
 pub fn is_implicit_attr(attr: &Attribute) -> bool {
     match &attr.meta {
         Meta::Path(path) => path.is_ident("implicit"),
         _ => false,
+    }
+}
+
+pub fn has_mut_pattern(pat: &Pat) -> bool {
+    let mut checker = MutChecker { has_mut: false };
+    checker.visit_pat(pat);
+    checker.has_mut
+}
+
+struct MutChecker {
+    has_mut: bool,
+}
+
+impl<'ast> Visit<'ast> for MutChecker {
+    fn visit_pat_ident(&mut self, node: &'ast PatIdent) {
+        if node.mutability.is_some() {
+            self.has_mut = true;
+        }
+        // Continue walking through the rest of the pattern
+        visit::visit_pat_ident(self, node);
     }
 }
