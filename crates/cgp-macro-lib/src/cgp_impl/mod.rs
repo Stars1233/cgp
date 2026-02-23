4489daa22cd7ef8216a26b6f82342de77b4d503e
Soares Chen <soares.chen@maybevoid.com>
2026-02-23 21:17:26 +0100

    Support `#[use_provider]` attribute inside `#[cgp_impl]` for higher order providers (#204)
    * Derive trait bounds in `#[use_provider]`
    
    * Support #[use_provider] inside method call expression

--- crates/cgp-macro-lib/src/cgp_impl/mod.rs
+++ crates/cgp-macro-lib/src/cgp_impl/mod.rs
@@ -1,9 +1,12 @@
 mod attributes;
 mod derive;
 mod implicit_args;
+mod provider_bounds;
+mod provider_call;
 mod provider_impl;
 mod spec;
 mod transform;
+mod use_provider;
 
 pub use derive::*;
 pub use provider_impl::*;
