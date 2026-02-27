8cf09a5229b35def07f48f49e995128f5f942c87
Soares Chen <soares.chen@maybevoid.com>
2026-02-27 23:11:15 +0100

    Support `#[use_provider]` inside `#[cgp_fn]` (#210)
    * Support `#[use_provider]` inside `#[cgp_fn]`
    
    * Test #[use_provider]
    
    * Remove use of `#[use_provider]` for method call

--- crates/cgp-macro-lib/src/cgp_impl/mod.rs
+++ crates/cgp-macro-lib/src/cgp_impl/mod.rs
@@ -2,13 +2,14 @@ mod attributes;
 mod derive;
 mod implicit_args;
 mod provider_bounds;
-mod provider_call;
 mod provider_impl;
 mod spec;
 mod transform;
 mod use_provider;
 
 pub use derive::*;
+pub use provider_bounds::*;
 pub use provider_impl::*;
 pub use spec::*;
 pub use transform::*;
+pub use use_provider::*;
