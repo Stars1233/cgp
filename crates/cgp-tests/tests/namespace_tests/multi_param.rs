598935bc484a282965a277a0bdae3d2d9ef9253e
Soares Chen <soares.chen@maybevoid.com>
2026-05-06 22:17:05 +0200

    Support impl-generic params in path keys and check values (#226)
    * Add test for multiple generic parameters
    
    * Support generic params before @ in path
    
    * Test generic params
    
    * Support generic check params in check_components!

---
+++ crates/cgp-tests/tests/namespace_tests/multi_param.rs
@@ -0,0 +1,58 @@
+use cgp::prelude::*;
+
+#[cgp_component(FooProvider)]
+#[use_namespace(@app.FooProviderComponent)]
+pub trait Foo<'a, T, U> {
+    fn foo(&self, first: &'a T, second: U);
+}
+
+#[cgp_impl(new DummyFoo)]
+impl<'a, T, U> FooProvider<'a, T, U> {
+    fn foo(&self, _first: &'a T, _second: U) {}
+}
+
+pub struct AppA;
+
+delegate_components! {
+    AppA {
+        open FooProviderComponent;
+
+        @FooProviderComponent.String.u32:
+            DummyFoo,
+        <T> @FooProviderComponent.bool.T:
+            DummyFoo,
+    }
+}
+
+check_components! {
+    AppA {
+        FooProviderComponent: [
+            <'a> (Life<'a>, String, u32),
+            <'a> (Life<'a>, bool, String),
+        ],
+        FooProviderComponent:
+            <'a> (Life<'a>, bool, bool),
+    }
+}
+
+pub struct AppB;
+
+delegate_components! {
+    AppB {
+        namespace default;
+
+        @app.FooProviderComponent.String.u64:
+            DummyFoo,
+        @app.FooProviderComponent.bool.<T> T:
+            DummyFoo,
+    }
+}
+
+check_components! {
+    <'a> AppB {
+        FooProviderComponent: [
+            (Life<'a>, String, u64),
+            (Life<'a>, bool, String),
+        ],
+    }
+}
